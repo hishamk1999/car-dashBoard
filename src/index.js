086a86d56bf63078869e88ee9591de350f4b5446
Hisham khaled <hishamk1999@gmail.com>
2023-01-07 14:42:32 +0200

    Installed Bootstrap v5.2.3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // Sass
 import "./sass/index.scss";
 
